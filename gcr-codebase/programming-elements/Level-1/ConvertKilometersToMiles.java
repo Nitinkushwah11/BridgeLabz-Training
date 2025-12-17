92528301c048632a4102ac9706e4e08300098d72
Nitinkushwah11 <nitinkush9587@gmail.com>
2025-12-17 16:00:09 +0530

    [nitin kushwah] Add. added ConvertKilometersToMiles.java file

---
+++ gcr-codebase/programming-elements/Level-1/ConvertKilometersToMiles.java
@@ -0,0 +1,10 @@
+public class ConvertKilometersToMiles
+{
+   public static void main(String args[])
+   {
+     double distance = 10.8;
+     double oneKM = 1.6;
+     double totalDistance = distance*oneKM;
+     System.out.println("The distance " + distance+ " km in miles is " + totalDistance);
+   }
+}
