1a378388f12af912d2e019f55cddebac109786d4
Nitinkushwah11 <nitinkush9587@gmail.com>
2025-12-20 13:39:24 +0530

    [nitin kushwah] : Added Handshakes file

---
+++ gcr-codebase/java-methods/Level-1/Handshakes.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Handshakes {
+
+    // Method to calculate number of handshakes
+    static int calculateHandshakes(int n) {
+        return (n * (n - 1)) / 2;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input number of students
+        System.out.print("Enter number of students: ");
+        int numberOfStudents = sc.nextInt();
+
+        // Method call
+        int handshakes = calculateHandshakes(numberOfStudents);
+
+        // Display result
+        System.out.println("Maximum number of handshakes = " + handshakes);
+
+        sc.close();
+    }
+}
