3fb3da41a5f6b9c310b3bb17686612b60e9dd872
Nitinkushwah11 <nitinkush9587@gmail.com>
2025-12-31 12:19:29 +0530

    [nitin kushwah] : Added Employee file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructors/level1/Employee.java
@@ -0,0 +1,24 @@
+package com.constructors.level1;
+/*
+ * Program Name : Employee
+ * Description  : Employee records with access control.
+ */
+class Employee {
+    public int employeeID;
+    protected String department;
+    private double salary;
+
+    Employee(int employeeID, String department, double salary) {
+        this.employeeID = employeeID;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+}
