da61bf065cc4f130ac2a67c48eb53cbb8c8226fe
Nitinkushwah11 <nitinkush9587@gmail.com>
2026-01-01 13:31:38 +0530

    [nitin kushwah] : Added Faculty file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/selfproblems/university/Faculty.java
@@ -0,0 +1,13 @@
+package com.objectmodeling.selfproblems.university;
+
+public class Faculty {
+    private String name;
+
+    public Faculty(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
