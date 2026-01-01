dd3c80052ec6c6643af77a88a127ba5362e9831b
Nitinkushwah11 <nitinkush9587@gmail.com>
2026-01-01 13:17:06 +0530

    [nitin kushwah] : Added Book file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/libraryandbooks/Book.java
@@ -0,0 +1,16 @@
+package com.objectmodeling.libraryandbooks;
+
+public class Book {
+
+    String title;
+    String author;
+
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    void displayBook() {
+        System.out.println("Title: " + title + ", Author: " + author);
+    }
+}
