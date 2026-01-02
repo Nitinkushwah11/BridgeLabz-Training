6551b6a8229e6ea8be1c5a2958813cbb2ca2dc00
Nitinkushwah11 <nitinkush9587@gmail.com>
2026-01-02 16:08:57 +0530

    [Nitin Kushwah] : Added Main file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/singleinheritance/librarymanagement/Main.java
@@ -0,0 +1,11 @@
+package com.inheritance.singleinheritance.librarymanagement;
+
+public class Main {
+	
+	public static void main(String[] args) 
+	{
+		Author author1 = new Author("Java",1995, "James Gosling", "Creator of Java programming");
+		author1.displayInfo();
+	}
+
+}
