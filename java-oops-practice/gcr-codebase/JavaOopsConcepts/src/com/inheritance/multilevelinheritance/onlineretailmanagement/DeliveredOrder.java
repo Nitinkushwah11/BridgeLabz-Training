70dbd15367948735e678a7f4ef5cef3a4af5bb9f
Nitinkushwah11 <nitinkush9587@gmail.com>
2026-01-02 16:02:50 +0530

    [Nitin Kushwah] : Added DeliveredOrder file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/multilevelinheritance/onlineretailmanagement/DeliveredOrder.java
@@ -0,0 +1,22 @@
+package com.inheritance.multilevelinheritance.onlineretailmanagement;
+
+public class DeliveredOrder extends ShippedOrder {
+    private String deliveryDate;
+
+    public DeliveredOrder(int orderId, String orderDate,
+                          String trackingNumber, String deliveryDate) {
+        super(orderId, orderDate, trackingNumber);
+        this.deliveryDate = deliveryDate;
+    }
+
+    @Override
+    public String getOrderStatus() {
+        return "Order Delivered";
+    }
+
+    @Override
+    public void display() {
+        super.display();
+        System.out.println("Delivered On: " + deliveryDate);
+    }
+}
