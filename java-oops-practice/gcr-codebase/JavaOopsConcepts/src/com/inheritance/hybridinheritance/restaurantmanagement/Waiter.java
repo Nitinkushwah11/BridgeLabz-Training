d8297dc908eb27478f18839241dcff975b119c39
Nitinkushwah11 <nitinkush9587@gmail.com>
2026-01-02 15:46:51 +0530

    [Nitin Kushwah] : Added Waiter file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hybridinheritance/restaurantmanagement/Waiter.java
@@ -0,0 +1,13 @@
+package com.inheritance.hybridinheritance.restaurantmanagement;
+
+public class Waiter extends Person implements Worker {
+
+    public Waiter(String name, int id) {
+        super(name, id);
+    }
+
+    @Override
+    public void performDuties() {
+        System.out.println("Duties : Taking orders and serving customers.");
+    }
+}
