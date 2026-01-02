d3f523eb703924c46d3ed501922d5d59bb48bbbf
Nitinkushwah11 <nitinkush9587@gmail.com>
2026-01-02 15:56:45 +0530

    [Nitin Kushwah] : Added MainCourseApp file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/multilevelinheritance/educationalcourse/MainCourseApp.java
@@ -0,0 +1,19 @@
+package com.inheritance.multilevelinheritance.educationalcourse;
+
+public class MainCourseApp {
+    public static void main(String[] args) {
+
+        PaidOnlineCourse course = new PaidOnlineCourse(
+                "Java Full Stack",
+                120,
+                "Udemy",
+                true,
+                5000,
+                20
+        );
+
+        System.out.println("----- COURSE DETAILS -----\n");
+        course.display();
+    }
+}
+
