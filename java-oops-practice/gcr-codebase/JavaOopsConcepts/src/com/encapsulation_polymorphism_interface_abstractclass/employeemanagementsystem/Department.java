086c5dadc1c2003201f0589e4e594ff07cfa29dd
Nitinkushwah11 <nitinkush9587@gmail.com>
2026-01-04 12:08:59 +0530

    [Nitin Kushwah] : Added Department file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation_polymorphism_interface_abstractclass/employeemanagementsystem/Department.java
@@ -0,0 +1,8 @@
+package com.encapsulation_polymorphism_interface_abstractclass.employeemanagementsystem;
+
+interface Department 
+ {
+	   void assignDepartment(String deptName);
+	   String getDepartmentDetails();	
+
+ }
