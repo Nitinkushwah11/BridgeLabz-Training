bab6a2c99dbef34fe93ca1dda578eaa8b1bb9049
Nitinkushwah11 <nitinkush9587@gmail.com>
2026-01-04 12:33:13 +0530

    [Nitin Kushwah] : Added Insurable file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation_polymorphism_interface_abstractclass/vehiclerentalsystem/Insurable.java
@@ -0,0 +1,6 @@
+package com.encapsulation_polymorphism_interface_abstractclass.vehiclerentalsystem;
+
+interface Insurable {
+    double calculateInsurance(int days);
+    String getInsuranceDetails();
+}
