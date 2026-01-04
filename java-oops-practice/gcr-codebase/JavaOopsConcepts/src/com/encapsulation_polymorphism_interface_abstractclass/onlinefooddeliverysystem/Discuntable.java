863aa45082dcc162a585a75c89b7c3cf647f698c
Nitinkushwah11 <nitinkush9587@gmail.com>
2026-01-04 12:25:14 +0530

    [Nitin Kushwah] : Added Discuntable file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation_polymorphism_interface_abstractclass/onlinefooddeliverysystem/Discuntable.java
@@ -0,0 +1,6 @@
+package com.encapsulation_polymorphism_interface_abstractclass.onlinefooddeliverysystem;
+
+interface Discountable {
+    double applyDiscount();
+    String getDiscountDetails();
+}
