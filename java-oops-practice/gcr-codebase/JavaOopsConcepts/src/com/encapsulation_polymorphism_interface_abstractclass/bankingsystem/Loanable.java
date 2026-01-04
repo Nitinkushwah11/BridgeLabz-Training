fc627555edf745b78cf85aa92c2bf42dfa4e44c2
Nitinkushwah11 <nitinkush9587@gmail.com>
2026-01-04 12:01:39 +0530

    [Nitin Kushwah] : Added Loanable file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation_polymorphism_interface_abstractclass/bankingsystem/Loanable.java
@@ -0,0 +1,6 @@
+package com.encapsulation_polymorphism_interface_abstractclass.bankingsystem;
+
+interface Loanable {
+    void applyForLoan(double amount);
+    double calculateLoanEligibility();
+}
