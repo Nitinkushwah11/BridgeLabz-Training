60eb065aede0c111ad9759b0ebffd5e4ec68a923
Nitinkushwah11 <nitinkush9587@gmail.com>
2026-01-04 12:30:14 +0530

    [Nitin Kushwah] : Added GPS file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation_polymorphism_interface_abstractclass/ridehailingapplication/GPS.java
@@ -0,0 +1,6 @@
+package com.encapsulation_polymorphism_interface_abstractclass.ridehailingapplication;
+
+interface GPS {
+    String getCurrentLocation();
+    void updateLocation(String newLocation);
+}
