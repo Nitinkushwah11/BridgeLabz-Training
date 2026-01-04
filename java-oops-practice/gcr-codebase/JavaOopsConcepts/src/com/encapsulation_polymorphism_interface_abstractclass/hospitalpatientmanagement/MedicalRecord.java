537d5bf33e96e1a277b26309cfb437fefc04a2b9
Nitinkushwah11 <nitinkush9587@gmail.com>
2026-01-04 12:17:30 +0530

    [Nitin Kushwah] : Added MedicalRecord file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation_polymorphism_interface_abstractclass/hospitalpatientmanagement/MedicalRecord.java
@@ -0,0 +1,6 @@
+package com.encapsulation_polymorphism_interface_abstractclass.hospitalpatientmanagement;
+
+interface MedicalRecord {
+    void addRecord(String record);
+    void viewRecords();
+}
