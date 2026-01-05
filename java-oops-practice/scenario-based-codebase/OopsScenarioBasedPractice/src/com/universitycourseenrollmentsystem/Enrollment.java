275793f6237cfa5dbb1ab6ef28a1a568bd01c4da
Nitinkushwah11 <nitinkush9587@gmail.com>
2026-01-05 11:13:11 +0530

    [Nitin Kushwah] : Added Enrollment file

---
+++ java-oops-practice/scenario-based-codebase/OopsScenarioBasedPractice/src/com/universitycourseenrollmentsystem/Enrollment.java
@@ -0,0 +1,12 @@
+package com.universitycourseenrollmentsystem;
+
+class Enrollment {
+    private Student student;
+    private Course course;
+
+    public Enrollment(Student student, Course course) {
+        this.student = student;
+        this.course = course;
+        System.out.println(student + " enrolled in " + course.getCourseName());
+    }
+}
