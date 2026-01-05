161471be2d3a5155625bbd6a56be18a19a42576c
Nitinkushwah11 <nitinkush9587@gmail.com>
2026-01-05 11:19:44 +0530

    [Nitin Kushwah] : Added Customer file

---
+++ java-oops-practice/scenario-based-codebase/OopsScenarioBasedPractice/src/com/vehiclerentalapplication/Customer.java
@@ -0,0 +1,15 @@
+package com.vehiclerentalapplication;
+
+class Customer {
+    private String name;
+    private String mobile;
+
+    public Customer(String name, String mobile) {
+        this.name = name;
+        this.mobile = mobile;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
