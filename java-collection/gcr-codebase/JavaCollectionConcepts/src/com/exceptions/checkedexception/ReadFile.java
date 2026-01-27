c6e00ea63a583bd9170ebd7ccdff1f08b4796643
Nitinkushwah11 <nitinkush9587@gmail.com>
2026-01-27 23:08:54 +0530

    [Nitin Kushwah] : Added ReadFile

---
+++ java-collection/gcr-codebase/JavaCollectionConcepts/src/com/exceptions/checkedexception/ReadFile.java
@@ -0,0 +1,24 @@
+package com.exceptions.checkedexception;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+public class ReadFile {
+
+	public static void main(String[] args )
+	{
+		try
+		{
+	    BufferedReader br = new BufferedReader(new FileReader("D:\\Java programming workspace\\java-collection\\gcr-codebase\\JavaCollectionConcepts\\src\\com\\exceptions\\checkedexception\\data.txt"));
+		String line;
+		while((line = br.readLine()) != null)
+		{
+		  System.out.println(line);
+		}
+		br.close();
+	   }catch(IOException e)
+	   {
+		  System.out.println("File not found");
+	   }
+	}
+}
